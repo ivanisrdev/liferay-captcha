6119787e1225de9ab4604439a4a7c6fcf5fd09ad
ivan.sanchez <ivan.sanchez@andorratelecom.ad>
2022-02-11 13:19:36 +0100

    [ADD] form-captcha-web: create module mvc-portlet for Liferay DXP 7.1

---
+++ modules/form-captcha-web/src/main/java/com/sample/form/captcha/web/constants/FormCaptchaWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.sample.form.captcha.web.constants;
+
+/**
+ * @author Ivan
+ */
+public class FormCaptchaWebPortletKeys {
+
+	public static final String FORMCAPTCHAWEB =
+		"com_sample_form_captcha_web_FormCaptchaWebPortlet";
+
+}
